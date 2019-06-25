2d97b3e1fd3ec4c0f249004d6a00369420e8ad68
Mujtaba Faizi <chaoticworld685@hotmail.com>
2019-06-25 02:12:24 -0700

    Refactor To-do.py : Changing datastructure to dictionary ; Added functionality of adding a task at an index

--- tintash/To-do.py
+++ tintash/To-do.py
@@ -2,12 +2,20 @@ tasks=[]
 
 def create():
     name = input("Enter Task name: ")
-    tasks.append([name,"incomplete"])   #default status is incomplete ; data-structure used is nested list
+    print("koko")
+    option = input("Do you wanna add the task at some index (y/n)? or simply at the end: ")
+    if option=='y':
+        show()
+        index = int(input("Enter the index: "))
+        tasks.insert(index,{"name": name, "status": "incomplete"})
+    else:
+        tasks.append({ "name": name,"status": "incomplete"})
+        #default status is incomplete ; data-structure used is dictionaries within list
 
 def show():
     count=0
     for t in tasks:
-        print("Task Index :",count, "\t\tTask name :",t[0], "\t\tTask status :",t[1])
+        print("Task Index :",count, "\t\tTask name :",t["name"], "\t\tTask status :",t["status"])
         count+=1
 
 def update():
@@ -17,22 +25,21 @@ def update():
     option=input("Do you want to change the name? (y/n) ")
     if option == 'y':
         name=input("Enter new name : ")
-        tasks[index][0] = name
-    if tasks[index][1] == "incomplete":
+        tasks[index]["name"] = name
+    if tasks[index]["status"] == "incomplete":
         option=input("Do you want to change the status to completed? (y/n) ")
         if option == 'y':
-            tasks[index][1] = "completed"
+            tasks[index]["status"] = "completed"
 
 def delete():
     show()
     index = input("Enter the task index for the task you want to delete : ")
     del tasks[int(index)]
 
-for a in range(1000):
+while True:
     option = input("1. Create a Task\n2. List tasks\n3. Update a Task\n4. Delete a Task \n0. Exit Program\n")
     option=int(option)
     if option==1:
-        print(option)
         create()
     elif option==2:
         show()
